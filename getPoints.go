4bf905c62d06af316538e12dcc3b111b0699c8b4
Guo, Jingfeng <jingfeng.guo@intel.com>
2020-10-19 13:04:29 +0800

    first commit

---
+++ getPoints.go
@@ -0,0 +1,39 @@
+package geoJson
+
+import (
+	"os"
+	"io/ioutil"
+	"encoding/json"
+	"errors"
+)
+
+func file_get_contents(path string) ([]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadALL(f)
+}
+
+func GetPoints(path string) ([]StructPoint, error) {
+	var content []byte
+	var err error
+	var c StructGeo
+	var res []StructPoint
+
+	content, err = file_get_contents(path)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal([]byte(content), &c)
+	if err != nil {
+		return nil, err
+	}
+	for _, points := range c.Features {
+		res = append(res, &StructPoint{
+			lng: points.Geometry.Coordinates[0],
+			lat: points.Geometry.Coordinates[1]
+		})
+	}
+	return res, nil
+}
---
+++ struct.go
@@ -0,0 +1,20 @@
+package geoJson
+
+type StructGeo struct {
+	Type string `json:"type"`
+	Features []*StructFeature `json:"features"`
+}
+
+type StructFeature struct {
+	Type string `json:"type"`
+	Geometry StructGeometry `json:"geometry"`
+}
+
+type StructGeometry struct {
+	Coordinates []*float64 `json:"coordinates"`
+}
+
+type StructPoint struct {
+	lat float64
+	lng float64
+}
